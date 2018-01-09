fb82dd5e97f04b4c09670d8269e63685a7425827
Alex Nassif <alexnassif@Alexs-MBP.home>
2018-01-08 18:23:24 -0800

    added pagecontrol

---
+++ WorkoutByWill/Controller/BeforeAfterImageVC.swift
@@ -0,0 +1,117 @@
+//
+//  BeforeAfterImageVC.swift
+//  WorkoutByWill
+//
+//  Created by Alex Nassif on 1/8/18.
+//  Copyright © 2018 alexnassif. All rights reserved.
+//
+
+import UIKit
+
+class BeforeAfterImageVC: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    lazy var imageViewControllers: [UIViewController] = {
+        
+        return [self.newVC(viewController: "beginImageVC"),
+                self.newVC(viewController: "afterImageVC")]
+    }()
+    
+    
+    var pageControl = UIPageControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.dataSource = self
+        self.delegate = self
+        
+        
+        // This sets up the first view that will show up on our page control
+        if let firstViewController = imageViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+        configurePageControl()        // Do any additional setup after loading the view.
+    }
+    
+    func newVC(viewController: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: viewController)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = imageViewControllers.index(of: pageContentViewController)!
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = imageViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        // User is on the first view controller and swiped left to loop to
+        // the last view controller.
+        guard previousIndex >= 0 else {
+            return imageViewControllers.last
+            // Uncommment the line below, remove the line above if you don't want the page control to loop.
+            // return nil
+        }
+        
+        guard imageViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return imageViewControllers[previousIndex]
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = imageViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let imageViewControllersCount = imageViewControllers.count
+        
+        // User is on the last view controller and swiped right to loop to
+        // the first view controller.
+        guard imageViewControllersCount != nextIndex else {
+            return imageViewControllers.first
+            // Uncommment the line below, remove the line above if you don't want the page control to loop.
+            // return nil
+        }
+        
+        guard imageViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return imageViewControllers[nextIndex]
+        
+    }
+    
+    func configurePageControl() {
+        // The total number of pages that are available is based on how many available colors we have.
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.minY + 200,width: UIScreen.main.bounds.width,height: 50))
+        self.pageControl.numberOfPages = imageViewControllers.count
+        self.pageControl.currentPage = 0
+        self.pageControl.tintColor = UIColor.black
+        self.pageControl.pageIndicatorTintColor = UIColor.white
+        self.pageControl.currentPageIndicatorTintColor = UIColor.black
+        self.view.addSubview(pageControl)
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
